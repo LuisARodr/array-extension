cf78614de2f1776ea033d4824f0624f894df5659
Luis Rodriguez <luis_angel_1521@hotmail.com>
2019-08-14 16:16:57 -0700

    Add tests for "each"

---
+++ test/each.js
@@ -0,0 +1,40 @@
+'use strict'
+
+var expect = require('chai').expect;
+var ArrayExtension = require('../Methods/ArrayExtension');
+
+
+describe('Array Extension', function() {
+  describe('#each()', function() {
+    it('should concatenate the values when each value is added to a empty sting', function() {
+      let array = [1, 2, 3, 4, 5];
+      let concatArray = '';
+
+      array.each(value => concatArray += value);
+
+      expect(concatArray).to.be.equal("12345");
+    });
+    it('should add the values of the array when the array values are numbers and the function adds them', function() {
+      let array = [1, 1, 2, 2, 4];
+      let sum = 0;
+
+      array.each(value => sum += value);
+
+      expect(sum).to.be.equal(10);
+    });
+    it('should execute the function the same number of times as the length of the array', function() {
+      let array = new Array(21);
+
+      let count = 0;
+
+      array.each(() => count += 1);
+
+      expect(count).to.be.equal(21);
+    });
+    it('should throw a Type error when the callback is not a function', function() {
+      expect([].each.bind([], "Hello")).to.throw(`Type error: Hello is not a function.`);      
+      expect([].each.bind([], {name: "John"})).to.throw(`Type error: [object Object] is not a function.`);    
+      expect([].each.bind([])).to.throw(`Type error: undefined is not a function.`);      
+    });
+  });
+});
