36bef7c3d45a30448645c80d0908fba04964551e
Luis Rodriguez <luis_angel_1521@hotmail.com>
2019-08-14 16:13:55 -0700

    Add method each to the ArrayExtension

---
+++ Methods/ArrayExtension.js
@@ -0,0 +1,16 @@
+'use strict'
+
+/**
+ * Executes a callback function for each value of the array.
+ * @param callback A function to be executed for each value of the array. Accepts a param this being the current 
+ * value of the array.
+ * @throws {TypeError} If the callback type is no a function
+ */
+Array.prototype.each = function call(callback) {
+    if ((typeof callback) != 'function'){
+        throw new TypeError(`Type error: ${callback} is not a function.`);
+    }
+    for (let arrValue of this) {
+        callback.call(this, arrValue)
+    }
+}
