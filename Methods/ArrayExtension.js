6929588e5c58426f19fb78a748ea24cf12c21a6d
Luis Rodriguez <luis_angel_1521@hotmail.com>
2019-08-15 08:40:18 -0700

    Add second param to the callback function

--- Methods/ArrayExtension.js
+++ Methods/ArrayExtension.js
@@ -2,15 +2,15 @@
 
 /**
  * Executes a callback function for each value of the array.
- * @param callback A function to be executed for each value of the array. Accepts a param this being the current 
- * value of the array.
+ * @param callback A function to be executed for each value of the array.
+ * Takes two params: the current value of the array and the index of the value.
  * @throws {TypeError} If the callback type is no a function
  */
 Array.prototype.each = function call(callback) {
     if ((typeof callback) != 'function'){
         throw new TypeError(`Type error: ${callback} is not a function.`);
     }
-    for (let arrValue of this) {
-        callback.call(this, arrValue);
+    for (let i = 0; i < this.length; i++) {
+        callback.call(this, this[i], i);
     }
 }
--- test/each.js
+++ test/each.js
@@ -31,6 +31,16 @@ describe('Array Extension', function() {
 
       expect(count).to.be.equal(21);
     });
+    it('should pass the current index with each function call', function() {
+      let array1 = [1,2,3,4,5];
+      let array2 = [1,2,3,4,5];
+
+      let isSameArrayValue = [];
+
+      array1.each((value, index) => isSameArrayValue.push( value == array2[index]) );
+
+      expect(isSameArrayValue).to.not.include(false);
+    })
     it('should throw a Type error when the callback is not a function', function() {
       expect([].each.bind([], "Hello")).to.throw(`Type error: Hello is not a function.`);      
       expect([].each.bind([], {name: "John"})).to.throw(`Type error: [object Object] is not a function.`);    
