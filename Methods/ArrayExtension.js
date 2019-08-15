7398ba0eb02d58d09ef9fcbf044cc5a8ca3c1f3b
Luis Rodriguez <luis_angel_1521@hotmail.com>
2019-08-15 12:57:11 -0700

    Add where method to ArrayExtension

--- Methods/ArrayExtension.js
+++ Methods/ArrayExtension.js
@@ -14,3 +14,27 @@ Array.prototype.each = function call(callback) {
         callback.call(this, this[i], i);
     }
 }
+
+/**
+ * Returns a new array from the elements of the array that satisfies the
+ * spec function
+ * @param spec A function that evaluates every value of the array, if spec
+ * returns true then the value is added to the resulting array, it takes 
+ * two params: the current value of the array and the index of the value.
+ * @throws {TypeError} If spec is no a function.
+ */
+Array.prototype.where = function where(spec) {
+    if ((typeof spec) != 'function') {
+        throw new TypeError(`Type error: ${spec} is not a function.`);
+    }
+
+    let result = [];
+
+    this.each(function evaluate(value, index) {
+        if (spec.call(this, value, index)) {
+            result.push(value);
+        }
+    });
+
+    return result;
+}
