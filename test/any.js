9b6c445274573af9a426e5125a318df3ef6ef80a
Luis Rodriguez <luis_angel_1521@hotmail.com>
2019-08-19 10:25:32 -0700

    Remove description

--- test/any.js
+++ test/any.js
@@ -4,15 +4,6 @@ var expect = require('chai').expect;
 var ArrayExtension = require('../Methods/ArrayExtension');
 
 
-/**
- * smal description for me, remove before PR
- * spec might be a function, then it evaluates every value of the array
- * 
- * if spec is not a function then it looks for a value that is the same on the array
- * 
- * in any case it returns true if theres a match, or false if there's no match 
- */
-
 describe('Array Extension', function() {
   describe('#any()', function() {
     it('should return false when the value is not found in the array', function() {
